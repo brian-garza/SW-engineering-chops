1814988160063e3b18f4fb2581ad400e41e649e3
Brian Garza <brian.garza@wgu.edu>
2023-11-08 17:58:16 -0500

    starting crow project

---
+++ crow.py
@@ -0,0 +1,45 @@
+# Script to find the distance a crow takes
+
+import pgeocode
+
+country = pgeocode.GeoDistance('us')
+
+start_zip = input("What zip code does the crow start?\n")
+end_zip = input ("Where does the crow end?\n")
+
+segment_length = country.query_postal_code(start_zip, end_zip)
+miles_seg_length = segment_length * float(0.621371)
+
+print(f'The crow has traveled {miles_seg_length:2,.2f} miles so far')
+
+####&&&&########&&&&########&&&&########&&&&########&&&&########&&&&####
+####&&&&########&&&&########&&&&########&&&&########&&&&########&&&&####
+####&&&&########&&&&########&&&&########&&&&########&&&&########&&&&####
+
+class Coordinates():
+    def __init__(self, start_coords, end_coords, latitude, longitude):
+
+        self.start_coords = start_coords
+        self.end_coords = end_coords
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def get_coords(self):
+        start_coords = {}
+        end_coords = {}
+        start_coords['lat']  = round(input("What is the starting latitude?\n"),2)
+        start_coords['long'] = round(input("What is the starting longitude?\n"),2)
+        end_coords['lat']  = round(input("What is the ending latitude?\n"),2)
+        end_coords['long'] = round(input("What is the ending longitude?\n"),2)
+
+class Segment(Coordinates):
+    def get_distance_of_segment(self, latitude, longitude):
+        pass
+
+class Trip(Segment):
+    def __init__(self, latitude, longitude):
+        pass
+
+    def getDistanceOfTrip(self):
+        pass
+
